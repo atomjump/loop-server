c8f2eb0416eacad7fde84b4203f58cf738ed8869
Apple <apple@Apples-Mac.fritz.box>
2016-08-05 15:42:30 +1200

    view count at 0 integer vs string

--- search-chat.php
+++ search-chat.php
@@ -34,7 +34,7 @@ if(($_SESSION['logged-user'] != '')&&(isset($_SESSION['logged-user']))) {
 	}
 } else {
 	
-	error.log("Server port:" .$_SERVER['SERVER_PORT'] . " logoutPort:" . $cnf['logoutPort'] . " view-count = " . intval($_SESSION['view-count']);
+	error.log("Server port:" .$_SERVER['SERVER_PORT'] . " logoutPort:" . $cnf['logoutPort'] . " view-count = " . intval($_SESSION['view-count']));
 		
 	//First request from new user, make sure we get the ip address
 	if(($_SERVER['SERVER_PORT'] == $cnf['logoutPort'])||   // this case is after a logout
