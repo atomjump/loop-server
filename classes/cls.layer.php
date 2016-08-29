49dd45f197cc2bbc543ce557849a6280071180d6
Apple <apple@Apples-Mac.fritz.box>
2016-08-29 16:49:44 +1200

    moved js

--- classes/cls.layer.php
+++ classes/cls.layer.php
@@ -695,7 +695,7 @@ class cls_login
 			    $this->get_group_user();
 			
 				//Only the owners can do this
-				if($_SESSION['logged-group-user'] == $_SESSION['layer-group-user']) {		
+				if(($_SESSION['logged-group-user'] != "")&&($_SESSION['logged-group-user'] == $_SESSION['layer-group-user'])) {		
 						//No password protection already - set it in this case
 						$sql = "UPDATE tbl_layer SET var_public_code = '" . md5(clean_data($full_request['setforumpassword'])) . "' WHERE int_layer_id = " . $layer_info['int_layer_id'];
 						dbquery($sql) or die("Unable to execute query $sql " . dberror());
