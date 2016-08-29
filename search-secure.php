3167ff60dddce51676b68ab92b391ecf55b50bcd
Apple <apple@Apples-Mac.fritz.box>
2016-08-29 15:22:14 +1200

    first attempt at access option for forums

--- search-secure.php
+++ search-secure.php
@@ -399,7 +399,7 @@
 			</div>
 			<div id="comment-prev-messages">
 			</div>
-			<div style="display: none; color: red;" id="forum-logged-in">
+			<div style="display: none; color: #800000;" id="forum-logged-in">
 			</div>
 
 		</div>
