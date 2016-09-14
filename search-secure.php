388e7c20f8414f1ce13b404180047db29f685974
AtomJump <peter@atomjump.com>
2016-09-14 17:31:50 +1200

    emojis

--- search-secure.php
+++ search-secure.php
@@ -535,9 +535,9 @@
 		</div>
 		
 		<div id="comment-emojis" class="comment-frm-scroller" style="width: <?php echo $_REQUEST['width'] ?>px; height: <?php echo $_REQUEST['height'] ?>px;">
-					
+				<div>	
 				 <?php $sh->call_plugins_emojis(null); //User added plugins here ?>
-				
+				</div>
 		</div>
 		
 		
