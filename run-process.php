a30a311c513005e1bf3361e73024b11b1f69b2d5
AtomJump <peter@atomjump.com>
2016-09-30 09:50:51 +1300

    emails run in parallel, and start only one parallel process to run through all of them

--- run-process.php
+++ run-process.php
@@ -4,10 +4,13 @@
 	//Usage:  php run_process.php [urlencoded, json_encoded array of shell commands to run]
 	
 	if($argv[1]) {
+		error_log($argv[1]);
 		$process_parallel = json_decode(urldecode($argv[1]));
 
+		
 		for($cnt = 0; $cnt < count($process_parallel); $cnt++) {
-	
+			
+			error_log($process_parallel[$cnt]);
 			$ret = shell_exec($process_parallel[$cnt]);
 	
 		}
