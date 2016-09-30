8a289a6eb43b89d5edbf55d1aa424451c1001587
AtomJump <peter@atomjump.com>
2016-09-30 13:09:57 +1300

    include the global

--- classes/cls.pluginapi.php
+++ classes/cls.pluginapi.php
@@ -307,7 +307,6 @@ class cls_plugin_api {
 		    global $cnf;
 		    $command = $cnf['phpPath'] . " " . $local_server_path . "run-process.php " . urlencode(json_encode($process_parallel));
 		    $cmd = "nohup nice -10 " . $command . " > /dev/null 2>&1 &"; 
-		    error_log($cmd);
 		    $ret = shell_exec($cmd);
 
 		
