0ab06bc17c3a923ea4fe20d082e50ffb714c9517
AtomJump <peter@atomjump.com>
2016-10-04 09:10:58 +1300

    testing issue with iphones

--- search-secure.php
+++ search-secure.php
@@ -562,7 +562,7 @@
 				function vidiOS()
 				{
 					//Show the iphone download
-					$('#video-chat-iframe-container').html("<a href='https://itunes.apple.com/no/app/appear.in-free-group-video/id878583078?mt=8' target=\"_parent\"><img alt='Get it on the Apple Appstore' src=\"https://atomjump.com/images/applestore_135x40.svg\" width=\"135\" height=\"40\" style=\"padding:10px;\" border=\"0\"></a></br><span style=\"color: white;\" style=\"padding:10px;\">Then enter the forum 'aj-<?php echo $_REQUEST['uniqueFeedbackId'] ?>' in the Appear.in app.</span>");
+					$('#video-chat-iframe-container').html("<a href='https://itunes.apple.com/no/app/appear.in-free-group-video/id878583078?mt=8' target=\"_parent\"><img alt='Get it on the Apple Appstore' src=\"https://atomjump.com/images/applestore_135x40.svg\" width=\"135\" height=\"40\" style=\"margin:10px;\" border=\"0\"></a></br><span style=\"color: white;\" style=\"margin:10px;\">Then enter the forum 'aj-<?php echo $_REQUEST['uniqueFeedbackId'] ?>' in the Appear.in app.</span>");
 				}
 				
 				function vidDeactivateIE8()
