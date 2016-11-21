0aef4e39eef398f3f6d431584eb9d63a7179ecdf
AtomJump <peter@atomjump.com>
2016-11-21 16:11:56 +1300

    handle error from server and can continue typing

--- search-secure.php
+++ search-secure.php
@@ -190,7 +190,7 @@
 
 				
 			</script>
-			<script type="text/javascript" src="<?php echo $root_server_url ?>/js/chat-inner-1.0.12.js"></script> 
+			<script type="text/javascript" src="<?php echo $root_server_url ?>/js/chat-inner-1.0.13.js"></script> 
 			
 			
 	</head>
