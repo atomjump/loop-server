69c556bcfc2eda2bab670bc6d66d6085e1e90a6c
AtomJump <peter@atomjump.com>
2017-01-18 11:22:36 +1300

    trying to remove the number versions

--- p2p-payment.php
+++ p2p-payment.php
@@ -85,13 +85,19 @@
 				
 				$redirecturl = "https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=" . urlencode($email_to) . "&lc=US&item_name=". urlencode($msg). "&amount=".urlencode($_REQUEST['amount']). "&currency_code=". $_REQUEST['currencyCode'] ."&button_subtype=services&no_note=0&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHostedGuest";
 				
-				echo file_get_contents($redirecturl);
-				exit(0);
-				//Old: header("Location: " . $redirecturl);
+				
+				//Good but testing with js below: header("Location: " . $redirecturl);
 			} else {
 				echo $msg['msgs'][$lang]['cannotVerifyForPayment'];
-			
+				exit(0);
 			}
 		}
 
 ?>
+<html>
+    <head>
+    	<script>
+    	window.location = "<?php echo $redirecturl ?>";
+    	</script>
+	</head>
+</html>
