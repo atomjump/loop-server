c6741163a9555555d882b2a5c17d75acfc298e4a
AtomJump <peter@atomjump.com>
2017-01-28 09:31:41 +1300

    first draft of switching to a different db based on input layer

--- config/db_connect.php
+++ config/db_connect.php
@@ -80,14 +80,17 @@
 		global $db_cnf;
 		$db_cnf = $cnf['db'];
 
-		
+		echo "Checking scaleup. Passcode=" . $_REQUEST['passcode'];
 
 		if((isset($db_cnf['scaleUp']))&&(isset($_REQUEST['passcode']))) {	
 			//We are scaling up
-			for($cnt = 0; $cnt< count($cnf['db']['scaleUp']); $cnt ++) {	
-				if(preg_match($cnf['db']['scaleUp'][$cnt]['labelRegExp'],$_REQUEST['passcode'], $matches) == true) {
+			echo " Scaling up";
+			for($cnt = 0; $cnt< count($db_cnf['scaleUp']); $cnt ++) {	
+				echo $cnt . " check label:" . $db_cnf['scaleUp'][$cnt]['labelRegExp'];
+				if(preg_match($db_cnf['scaleUp'][$cnt]['labelRegExp'],$_REQUEST['passcode'], $matches) == true) {
 					//Override with this database
-					$db_cnf = $cnf['db']['scaleUp'][$cnt];
+					echo "Overriding with " . $cnt;
+					$db_cnf = $db_cnf['scaleUp'][$cnt];
 					return;
 				}
 
