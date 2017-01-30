1da5a5ff304d2a9bdb52e4153705709a90ebe55a
AtomJump <peter@atomjump.com>
2017-01-30 20:52:37 +1300

    testing server url

--- copy-image.php
+++ copy-image.php
@@ -3,8 +3,10 @@
 	
 	$encoded_file=$_POST['file'];
 	$decoded_file=base64_decode($encoded_file);
-	$dest = getcwd() . "/images/property/".$_POST['FILENAME'];
+	$dest = getcwd() . "/images/property/" . $_POST['FILENAME'];
+	
 	error_log("Destination for image " . $_POST['FILENAME'] . " is " . $dest);
+	echo "Destination for image " . $_POST['FILENAME'] . " is " . $dest;
 	
 	/*Now you can copy the uploaded file to your server.*/
 	file_put_contents($dest,$decoded_file);
