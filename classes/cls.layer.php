3295bd5ec3aa90562e8e7a62159cb721ebd1af74
AtomJump <peter@atomjump.com>
2017-02-01 11:25:53 +1300

    testing group save

--- classes/cls.layer.php
+++ classes/cls.layer.php
@@ -830,7 +830,7 @@ class cls_login
 	public function confirm($email, $password, $phone, $users = null, $layer_visible = null, $readonly = false, $full_request)
 	{
 		
-		error_log("Email: " . $email . " pass:" . $password . " layervis:" . $layer_visible . " readonly:" . $readonly . "  fullrequest:" . print_r($full_request));
+		error_log("Email: " . $email . " pass:" . $password . " layervis:" . $layer_visible . " readonly:" . $readonly);
 		
 		//Returns: [string with status],[RELOAD option - must be RELOAD],[user id] 
 		//
