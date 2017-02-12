1554d3beafb925944789c354407e0cdf05f95c8d
AtomJump <peter@atomjump.com>
2017-02-12 17:29:08 +1300

    global upload switch

--- search-secure.php
+++ search-secure.php
@@ -464,11 +464,13 @@
 				 </form>
 		</div>
 		<div id="comment-upload" class="comment-frm-scroller" style="width: <?php echo $_REQUEST['width'] ?>px; height: <?php echo $_REQUEST['height'] ?>px;">
+				<?php global $cnf; if($cnf['uploads']['use'] != 'none') { ?>
 				<h4><?php echo $msg['msgs'][$lang]['uploadTitle'] ?></h4>
-				
+				<?php } ?>
 				
 					
 				 <form id="upload-frm" class="form" role="form" action="" onsubmit="return upload();"  method="POST">
+				 		<?php global $cnf; if($cnf['uploads']['use'] != 'none') { ?>	
 				 				 <div class="form-group">
 				 						<div><?php echo $msg['msgs'][$lang]['selectImage'] ?></div>
 							 			<input id="image" name="fileToUpload" type="file" accept=".jpg,.jpeg," class="form-control" placeholder="<?php echo $msg['msgs'][$lang]['selectImagePrompt'] ?>" >
@@ -477,11 +479,12 @@
 								<div id="uploading-msg" style="display: none; color: #900; margin-bottom: 10px;"></div>
 								
 											 <button type="submit" class="btn btn-primary" style="margin-bottom:3px;" name="submit"><?php echo $msg['msgs'][$lang]['uploadButton'] ?></button>
-						  	<br/>
+						  	 <br/>
 							 <br/>
+							 
 							 <div><?php echo $msg['msgs'][$lang]['uploadLimits'] ?></div>
 							 <br/>
-								 
+					   <?php } ?>			 
 								 	<h4><?php echo $msg['msgs'][$lang]['downloadTitle'] ?></h4>
 								  <div><?php echo $msg['msgs'][$lang]['downloadDescription'] ?> 	</div>
 								  <br/>
