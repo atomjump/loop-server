0430e6ce70977da07de391b0021ed855f364eb45
AtomJump <peter@atomjump.com>
2018-06-20 12:08:02 +1200

    removing error logs

--- config/db_connect.php
+++ config/db_connect.php
@@ -372,9 +372,7 @@
 		global $process_parallel;
 		global $cnf;
 		
-		
-		error_log("Notify before sending mail: " . $notify);
-		
+				
 		if($notify == true) {
 			$cmd = 'nohup nice -n 10 ' . $cnf['phpPath'] .  ' ' . $local_server_path . 'send-email.php to=' . rawurlencode($to_email) . '  subject=' . rawurlencode($subject) . ' body=' . rawurlencode($body_text) . ' sender_email=' . rawurlencode($sender_email) .  ' sender_name=' . urlencode($sender_name) . ' to_name=' . urlencode($to_name) . ' staging=' . $staging . ' bcc=' . rawurlencode($bcc_email) . ' > /dev/null 2>&1 &';	//To log eg.: . ' >/var/www/html/atomjump_staging/tmp/newlog.txt';
 						
