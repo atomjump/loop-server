87e8dead317586b12dbe5169b1ff261c968eef8e
AtomJump <peter@atomjump.com>
2018-12-03 14:58:35 +1300

    order

--- search-secure.php
+++ search-secure.php
@@ -219,7 +219,7 @@
 			<div id="video-chat-container" style="position: relative; width: <?php echo $width ?>px; margin-left: auto; margin-right: auto; display: none; margin-bottom: 10px; display: none; background-color: #444;">
 				<div id="video-chat-iframe-container" style="width:<?php echo $width; ?>px; height: <?php echo $maxheight ?>px;"><iframe id="video-chat" style="z-index: 1000;" width="100%" height="100%" frameborder="0"></iframe></div>
 				<div style="position: relative; float: right; padding: 5px;  z-index: 5000;"><a href="javascript:" title="<?php echo $msg['msgs'][$lang]['goFullscreen'] ?>" onclick="toggleVideoFullScreen(); return false;"><img src="images/largerscreen.svg"></a></div>
-				<div style="position: relative; margin-top: 6px; margin-bottom: 6px; float: right; margin-right: 10px; text-align: right; color: white; opacity: 0.7; background-color: black; padding: 5px;border-radius: 5px; z-index: 5000;">Link: <?php if($cnf['video']['url']) {	echo str_replace("[FORUM"], $_REQUEST['uniqueFeedbackId'], $cnf['video']['url']); } else { echo "https://meet.jit.si/aj-changeme-" . $_REQUEST['uniqueFeedbackId']; } ?></div> 
+				<div style="position: relative; margin-top: 6px; margin-bottom: 6px; float: right; margin-right: 10px; text-align: right; color: white; opacity: 0.7; background-color: black; padding: 5px;border-radius: 5px; z-index: 5000;">Link: <?php if($cnf['video']['url']) {	echo str_replace("[FORUM]", $_REQUEST['uniqueFeedbackId'], $cnf['video']['url']); } else { echo "https://meet.jit.si/aj-changeme-" . $_REQUEST['uniqueFeedbackId']; } ?></div> 
 				<div style="clear: both; height: 2px;"></div>
 			</div>
 			
@@ -388,7 +388,7 @@
 									<button type="submit" id="private-button"  class="btn btn-info" style="margin-bottom:3px; display: none;"><?php echo $msg['msgs'][$lang]['sendPrivatelyButton'] ?></button>
 									<button type="submit" id="public-button" class="btn btn-primary" style="margin-bottom:3px;"><?php echo $msg['msgs'][$lang]['sendButton'] ?></button>
 									<a href="javscript:" style="white-space: nowrap; margin-left:3px;" onclick="return switchPublic();" id="private-public-link"><?php echo $msg['msgs'][$lang]['sendSwitchToPrivate'] ?></a>
-									<a target="_blank" href="<?php if($cnf['video']['url']) {	echo str_replace("[FORUM"], $_REQUEST['uniqueFeedbackId'], $cnf['video']['url']); } else { echo "https://meet.jit.si/aj-changeme-" . $_REQUEST['uniqueFeedbackId']; }" onclick="if(iOS == true) { return false; } else { return true; }; ?>" style="margin-bottom:3px;"><img id="video-button" src="<?php echo $root_server_url ?>/images/video.svg" title="Video Chat" style="width: 48px; height: 32px;"></a>
+									<a target="_blank" href="<?php if($cnf['video']['url']) {	echo str_replace("[FORUM]", $_REQUEST['uniqueFeedbackId'], $cnf['video']['url']); } else { echo "https://meet.jit.si/aj-changeme-" . $_REQUEST['uniqueFeedbackId']; }" onclick="if(iOS == true) { return false; } else { return true; }; ?>" style="margin-bottom:3px;"><img id="video-button" src="<?php echo $root_server_url ?>/images/video.svg" title="Video Chat" style="width: 48px; height: 32px;"></a>
 								</div>
 							
 							<?php } else { //No access so far - need to log in with the forum password ?>
