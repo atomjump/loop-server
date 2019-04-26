e0946daa4fe92be2521aa2c189c3b482dae33ead
atomjump <peter@atomjump.com>
2019-04-27 10:23:11 +1200

    adding a user subscription

--- search-secure.php
+++ search-secure.php
@@ -391,7 +391,7 @@
 									<?php global $cnf;
 										 $admin_user_id = explode(":",$cnf['adminMachineUser']);
 						
-										 if(($_SESSION['logged_user'])&&($_SESSION['logged_user'] == $admin_user_id)) {
+										 if(($_SESSION['logged_user'])&&($_SESSION['logged_user'] == $admin_user_id[1])) {
 										 	//Show a set forum password option ?>
 										<div id="set-forum-password-form" class="form-group" style="display:none;">
 											<div><?php echo $msg['msgs'][$lang]['setForumPass'] ?> <a href="javascript:" onclick="$('#forum-password-explain').slideToggle();" title="<?php echo $msg['msgs'][$lang]['setForumPassReason'] ?>"><?php echo $msg['msgs'][$lang]['optional'] ?></a>  <span id="forum-password-explain" style="display: none;  color: #f88374;"><?php echo $msg['msgs'][$lang]['setForumPassReasonExtended'] ?></span></div>
