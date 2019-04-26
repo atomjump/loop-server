72b60ed6a2c38adf7a6a21cf6965d51193d6eca5
atomjump <peter@atomjump.com>
2019-04-27 09:17:42 +1200

    adding a user subscription

--- classes/cls.layer.php
+++ classes/cls.layer.php
@@ -1080,7 +1080,7 @@ class cls_login
 				if($layer_info) {
 					//Yes the layer exists
 					$current_subs = $this->get_subscription_string($layer_info['int_layer_id']);
-					$this->add_to_subscriptions($current_subs, $layer_info['int_layer_id');			
+					$this->add_to_subscriptions($current_subs, $layer_info['int_layer_id']);			
 				}
 			
 			}
