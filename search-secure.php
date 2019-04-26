4a84eba6362786b1060eeea1e1ae1b5c48aac982
atomjump <peter@atomjump.com>
2019-04-27 09:49:46 +1200

    adding a user subscription

--- search-secure.php
+++ search-secure.php
@@ -366,7 +366,11 @@
 								<div id="comment-password-vis" style="display: none;">
 									<div  class="form-group">
 										<div><?php echo $msg['msgs'][$lang]['yourPassword'] ?> <a href="javascript:" onclick="$('#password-explain').slideToggle();" title="<?php echo $msg['msgs'][$lang]['yourPasswordReason'] ?>"><?php echo $msg['msgs'][$lang]['optional'] ?></a>, <a id='clear-password' href="javascript:" onclick="return clearPass();"><?php echo $msg['msgs'][$lang]['resetPasswordLink'] ?></a> <span id="password-explain" style="display: none; color: #f88374;"><?php echo $msg['msgs'][$lang]['yourPasswordReason'] ?> </span></div>
-						  				<input  onclick="$('#save-button').html('<?php echo $msg['msgs'][$lang]['loginSettingsButton'] ?>');" id="password-opt" name="pd" type="password" class="form-control" autocomplete="false" placeholder="<?php echo $msg['msgs'][$lang]['enterPassword'] ?>" value="">
+						  				<input oninput="if(this.value.length > 0) { $('#save-button').html('<?php if($msg['msgs'][$lang]['loginSettingsButton']) {
+		 echo $msg['msgs'][$lang]['loginSettingsButton']; 
+		} else { 
+			echo $msg['msgs'][$lang]['saveSettingsButton'];
+		} ?>'); } else { $('#save-button').html('<?php echo $msg['msgs'][$lang]['saveSettingsButton'] ?>'); }" id="email-opt" name="email-opt" type="email" class="form-control" placeholder="<?php echo $msg['msgs'][$lang]['enterEmail'] ?>" id="password-opt" name="pd" type="password" class="form-control" autocomplete="false" placeholder="<?php echo $msg['msgs'][$lang]['enterPassword'] ?>" value="">
 									</div>
 									<div  class="form-group">
 										<?php global $cnf; if($cnf['sms']['use'] != 'none') { ?>
