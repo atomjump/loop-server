005cd6143fa2e8dd4178f6f26ef01e44ae79ae18
atomjump <peter@atomjump.com>
2019-04-29 09:29:11 +1200

    a few tweaks

--- classes/cls.layer.php
+++ classes/cls.layer.php
@@ -1233,6 +1233,7 @@ class cls_login
 			//Yes the layer exists
 			if($layer_info['var_public_code']) {
 					if(isset($forum_password)) {
+						error_log("Forum pass:" . $forum_password .  "  layer code:" .$layer_info['var_public_code']);
 						if(md5(clean_data($forum_password)) != $layer_info['var_public_code']) {
 							return "FAILURE";
 						}
