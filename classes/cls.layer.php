ff069e916ade6938dcea2dc7bd40d995e5ed2a58
atomjump <peter@atomjump.com>
2019-05-17 15:00:29 +1200

    title to messaging

--- classes/cls.layer.php
+++ classes/cls.layer.php
@@ -1121,6 +1121,11 @@ class cls_login
 	    //Get the current layer - use to view 
 	    if(($email != "")&&($password == "")) {
 	    	//This is a subscription case: an email has been entered, but no password.
+	    	
+	    	//Log out by default
+	    	$_SESSION['logged-user'] = '';
+	    	$_SESSION['logged-email'] = '';
+	    	
 	    	$ly = new cls_layer(); 
 			$ip = $ly->getFakeIpAddr();  //get new user's ip address	
 				
