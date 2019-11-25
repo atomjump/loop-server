84e946f45c4a1c58ddbcd99d4a2237cd5bb78114
AtomJump <peter@atomjump.com>
2019-11-26 12:07:24 +1300

    resizing images client side

--- upload-photo.php
+++ upload-photo.php
@@ -4,7 +4,8 @@
 
 	//TESTING IMMEDIATE
 	$target_dir = "/images/im/";
-	print_r($_FILES["fileToUpload"]);
+	print_r($_FILES);
+	
 	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 	$uploadOk = 1;
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
