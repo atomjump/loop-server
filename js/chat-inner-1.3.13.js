05d89170be346e05c9a8021725b23b5b16d7491f
AtomJump <peter@atomjump.com>
2019-11-27 07:34:38 +1300

    upload and submit images immediately

--- js/chat-inner-1.3.13.js
+++ js/chat-inner-1.3.13.js
@@ -1072,7 +1072,7 @@ function upload() {
     //Handle each upload, with a 2 second delay between starting each one
     for(var cnt = 0; cnt< upload.length; cnt++) {
     
-    	var passInId = upload[cnt].value;
+    	var passInId = cnt;
     
     	setTimeout(function() {
     		alert(passInId);		//TESTING
