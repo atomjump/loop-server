b39f69f96e0b2c2d7aa7048409212daf7c4521df
AtomJump <peter@atomjump.com>
2019-12-02 10:47:19 +1300

    upload button hidden initially

--- search-secure.php
+++ search-secure.php
@@ -666,7 +666,7 @@
 			  	  previewFull.appendChild(canvasb);
   
 					function fullscreen(){
-							   var el = document.getElementById('previewFull');
+							   var el = document.getElementById('preview-full');
  
 							   if(el.webkitRequestFullScreen) {
 								   el.webkitRequestFullScreen();
