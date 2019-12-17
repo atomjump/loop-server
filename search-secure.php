ac3d7ae9f3c23d2418a7a1f7a6d3b4e57ff3fa4d
AtomJump <peter@atomjump.com>
2019-12-18 12:28:51 +1300

    with forum automatic titles

--- search-secure.php
+++ search-secure.php
@@ -862,7 +862,6 @@
 						vidDeactivate();
 					}
 					
-					/* TESTING OUT
 					if(ajFeedback.domain) {
 						var targetOrigin = decodeURIComponent(ajFeedback.domain);
 						
@@ -878,7 +877,6 @@
 						setOriginDomain(targetOrigin);
 					}
 					parent.postMessage( {'title': '<?php if(isset($layer_info['var_title'])) { echo $layer_info['var_title']; } ?>'}, targetOrigin );
-					*/
 					
 					
 					
