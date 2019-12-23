f83e0a80d962e9ef6d5370e2f891a15a191aa157
AtomJump <peter@atomjump.com>
2019-12-24 10:57:47 +1300

    forum logged in generic

--- search-secure.php
+++ search-secure.php
@@ -245,7 +245,7 @@
 
 				
 			</script>
-			<script type="text/javascript" src="<?php echo $root_server_url ?>/js/chat-inner-1.3.20.js"></script> 
+			<script type="text/javascript" src="<?php echo $root_server_url ?>/js/chat-inner-1.3.21.js"></script> 
 			
 		
 			
