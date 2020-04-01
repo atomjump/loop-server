5a75f3cd5acfdf13584ba72678e43b069d26d980
AtomJump <peter@atomjump.com>
2020-04-02 11:17:27 +1300

    allow optional subdomain replacement

--- search-secure.php
+++ search-secure.php
@@ -23,15 +23,19 @@
 	
 	}
 	
-	//This may be used for modifying the fast server web address
-	$subdomain = check_subdomain();
+
 	
-	if($cnf['readURLAllowReplacement']) {
-		$read_url = trim_trailing_slash(str_replace('[subdomain]', $subdomain, $cnf['readURL']));
-	} else {
-		$read_url = trim_trailing_slash(str_replace('[subdomain]', "", $cnf['readURL']));		//Remove any mention of subdomains
-	}
-	
+	if(isset($cnf['readURL'])) {
+			//This may be used for modifying the fast server web address
+			$subdomain = check_subdomain();
+
+
+			if((isset($cnf['readURLAllowReplacement'])) && ($cnf['readURLAllowReplacement' == true)) {
+				$read_url = trim_trailing_slash(str_replace('[subdomain]', $subdomain, $cnf['readURL']));
+			} else {
+				$read_url = trim_trailing_slash(str_replace('[subdomain]', "", $cnf['readURL']));		//Remove any mention of subdomains
+			}
+	}	
 	
 	function currentdir($url) {
 		// note: anything without a scheme ("example.com", "example.com:80/", etc.) is a folder
@@ -217,7 +221,7 @@
 				
 				<?php if(isset($cnf['readURL'])) { //Support a specific URL for fast reading with the loop-server-fast plugin 
 				?>
-					var readURL = "<?php echo $readURL; ?>";
+					var readURL = "<?php echo $read_url; ?>";
 				<?php } else { ?>
 					var readURL = null;
 				<?php } ?>
