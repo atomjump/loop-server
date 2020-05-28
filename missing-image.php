775704f45c8e03c4b27c3085c81739ce907c7971
AtomJump <peter@atomjump.com>
2020-05-28 14:07:46 +1200

    1st attempt at missing image handling

--- missing-image.php
+++ missing-image.php
@@ -25,7 +25,7 @@
 						//Only do with http
 						$server_url = str_replace("https", "http", $server_url);
 					}
-					error_log("server url:" . $server_url);   //TESTING
+					//error_log("server url:" . $server_url);   //TESTING
 					$servers[] = $server_url;
 					
 				}
@@ -45,12 +45,12 @@
 			
 			//$filename is now e.g. "upl-123443.jpg"
 			
-			$random_count = count($servers);
+			$random_count = count($servers) - 1;
 			
 			//Maximum 3 attempts from different cluster servers
 			for($cnt = 0; $cnt < 3; $cnt++) {
 				$random_server = rand(0, $random_count);
-				error_log("rand server:" . $random_server);   //TESTING
+				//error_log("rand server:" . $random_server);   //TESTING
 				
 				$server = $servers[$random_server];
 		
@@ -58,7 +58,7 @@
 					$url  = trim_trailing_slash($server) . "/images/im/" . $filename;
 					//E.g. $url = "https://staging.atomjump.com/api/images/im/upl440-47456560.jpg";
 					
-					error_log("url:" . $url);   //TESTING
+					//error_log("url:" . $url);   //TESTING
 					
 					$img = __DIR__ . '/images/im/' . $filename;
 					
