514581b75b48251fb06e0590c243304e51c9eeee
AtomJump <peter@atomjump.com>
2020-05-28 12:09:47 +1200

    1st attempt at missing image handling

--- missing-image.php
+++ missing-image.php
@@ -2,5 +2,10 @@
 
 	echo "This is a missing image served up by PHP";
 	echo "Caller:" . $_SERVER['REQUEST_URI'];
+	
+	$image = "https://staging.atomjump.com/api/images/im/upl440-47456560.jpg";
+	header('Content-type: image/jpeg');
+	readfile($image);
+
 
 ?>
