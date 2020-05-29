9263d4176563a23ac8eac795ac22dceb5efc609d
AtomJump <peter@atomjump.com>
2020-05-30 11:31:08 +1200

    image-exists

--- image-exists.php
+++ image-exists.php
@@ -1,9 +1,32 @@
 <?php
-	include_once(__DIR__ . '/config/db_connect.php');		
-	
-	global $local_server_path;
-	global $root_server_url;
-	global $cnf;
+	//This section is a simplified cnf getter - for speed.	
+	if(!isset($config)) {
+     //Get global config - but only once
+     $data = file_get_contents (dirname(__FILE__) . "/config.json");
+     if($data) {
+        $config = json_decode($data, true);
+        if(!isset($config)) {
+          echo "Error: config/config.json is not valid JSON.";
+          exit(0);
+        }
+     
+     } else {
+       echo "Error: Missing config/config.json.";
+       exit(0);
+     
+     } 
+  }
+  
+  if(($_SERVER["SERVER_NAME"] == $config['staging']['webDomain'])||($staging == true)||($config['usingStaging'] == true)) {
+		//Staging
+		$staging = true;
+		
+		$cnf = $config['staging'];
+  } else {
+  		$cnf = $config['production'];
+  
+  }
+  //Up until here
 	
 	
 	if($_REQUEST['code'] == $cnf['uploads']['imagesShare']['checkCode']) {
