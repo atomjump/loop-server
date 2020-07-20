01da8f625f2a075a8d8895c2cbb069daae021618
AtomJump <peter@atomjump.com>
2020-07-21 11:04:46 +1200

    a little testing

--- classes/cls.ssshout.php
+++ classes/cls.ssshout.php
@@ -959,7 +959,7 @@ class cls_ssshout
 				
 		
 		//Modify the username for input into the forum
-		$processed_name = clean_data($your_name);  //Or perhaps we need to do something similar to: preg_replace("/[^a-z0-9\_\-\.\ ]/i", '', $your_name);    //Remove special chars
+		$processed_name = $your_name;  //Or perhaps we need to do something similar to: preg_replace("/[^a-z0-9\_\-\.\ ]/i", '', $your_name);    //Remove special chars
 
 		//Make sure we differentiate two or more different usernames that are the same. E.g 'John', 'john (02)', 'John (03)'
 		$visible_name = $processed_name; //TEMPOUT$this->check_duplicate_names($processed_name, $user_id, $layer);
