9373dfd68329f3a217e1862ec02e99ee9a17dd46
AtomJump <peter@atomjump.com>
2020-07-21 13:27:17 +1200

    a little testing

--- classes/cls.ssshout.php
+++ classes/cls.ssshout.php
@@ -900,7 +900,7 @@ class cls_ssshout
 		
 
 		
-		$sql = "INSERT INTO tbl_multiuser_check SELECT NULL, var_username, int_author_id FROM tbl_ssshout WHERE enm_active = 'true' AND int_layer_id = " . $layer_id . " AND var_username = '" . $username . "' GROUP BY int_author_id ORDER BY int_ssshout_id DESC";
+		$sql = "INSERT INTO tbl_multiuser_check SELECT NULL, var_username, int_author_id FROM tbl_ssshout WHERE enm_active = 'true' AND int_layer_id = " . $layer_id . " AND var_username = '" . $username . "' GROUP BY int_author_id ORDER BY int_ssshout_id";
 		error_log($sql);			//TESTING
 	
 		$result = dbquery($sql)  or die(error_log("Unable to execute query $sql " . dberror()));
