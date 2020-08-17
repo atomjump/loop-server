e85b0f6cdcabee4795f72bf638dbdfd5116e2d12
AtomJump <peter@atomjump.com>
2020-08-17 12:09:44 +1200

    did not need the number in this json

--- js/chat-inner-1.3.28.js
+++ js/chat-inner-1.3.28.js
@@ -17,13 +17,7 @@ var lsmsg = {
               "blankMessage": "Warning: you tried to send a blank message.",
               "messageQueued": "Warning: your message 'MESSAGE' will be sent when a connection is re-established.",
               "subscribed": "You have successfully subscribed.",
-              "subscriptionDenied": "Sorry, you are not authorised to subscribe to this forum.",
-              "numbers": [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-            			 "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
-            			 "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
-            			 "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
-            			 "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
-            			 "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"]           
+              "subscriptionDenied": "Sorry, you are not authorised to subscribe to this forum."         
         },
         "es":{
               "defaultYourName": "Tu Nombre",
