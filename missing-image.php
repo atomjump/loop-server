24e8a578e3bf5304793bc45ecc860676bd2eb6b1
AtomJump <peter@atomjump.com>
2020-09-16 09:23:22 +1200

    testing image share with some additional ips

--- missing-image.php
+++ missing-image.php
@@ -64,7 +64,8 @@
 			
 			//$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 			
-			$image_path = trim_trailing_slash($server) . "/images";
+			$image_path = trim_trailing_slash($cnf['webRoot']) . "/images";
+			if($verbose == true) error_log("image path:" . $image_path);
 			$path = str_replace($image_path, "", $_SERVER['REQUEST_URI']);
 			$path = parse_url($path, PHP_URL_PATH);
 			if($verbose == true) error_log("path:" . $path);
