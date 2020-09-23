4ca6ac69ac2e78104efc6169222f4dc59088d357
AtomJump <peter@atomjump.com>
2020-09-24 10:03:55 +1200

    checking response from parallel

--- run-process.php
+++ run-process.php
@@ -3,6 +3,9 @@
 	//Runs through a series of shell commands in the background
 	//Usage:  php run_process.php [urlencoded, json_encoded array of shell commands to run]
 	
+	$debug_parallel = true;		//Usually false, switch to 'true' when debugging output from parallel processes
+
+	
 	if($argv[1]) {
 		$process_parallel = json_decode(urldecode($argv[1]));
 
@@ -10,7 +13,9 @@
 		for($cnt = 0; $cnt < count($process_parallel); $cnt++) {
 			
 			$ret = shell_exec($process_parallel[$cnt]);
-	
+			if($debug_parallel == true) {
+				error_log($ret);
+			}
 		}
 	} else {
 		echo "Sorry, you need to pass a urlencoded, json_encoded array of shell commands to run.";
