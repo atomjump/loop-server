4054de6b8610de118678ac98c96ee5f14d25f9d6
AtomJump <peter@atomjump.com>
2020-10-17 11:37:07 +1300

    update indexes one-time

--- update-indexes.php
+++ update-indexes.php
@@ -12,7 +12,8 @@ require("classes/cls.ssshout.php");
 	if(!file_exists($maiden_check)) {
 		file_put_contents($maiden_check, "Indexes have been run once.");
 		$sql = "CREATE INDEX ordered_ssshout_id ON tbl_ssshout (enm_active, int_layer_id, date_when_shouted, int_ssshout_id, var_whisper_to, var_ip); CREATE INDEX ordered_ssshout_big_id ON tbl_ssshout (enm_active, int_layer_id, date_when_shouted, int_ssshout_id, var_whisper_to, var_ip, int_author_id, int_whisper_to_id); ALTER TABLE tbl_ssshout DROP INDEX ordered_ssshout_full; ALTER TABLE tbl_ssshout DROP INDEX ordered_ssshout_big;";
-dbquery($sql);
+		dbquery($sql) or die("Sorry there was an error: unable to execute query. " . dberror());
+		
 	}
 
 ?>
