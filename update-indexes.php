6271e10f0ae95f3ed41eefcda89bdab1bfc6aff4
AtomJump <peter@atomjump.com>
2020-10-17 11:39:57 +1300

    update indexes one-time

--- update-indexes.php
+++ update-indexes.php
@@ -10,9 +10,21 @@ require("classes/cls.ssshout.php");
 	//Allowed to run this script once.
 	$maiden_check = "config/indexes_changed_0.txt";
 	if(!file_exists($maiden_check)) {
-		file_put_contents($maiden_check, "Indexes have been run once.");
-		$sql = "CREATE INDEX ordered_ssshout_id ON tbl_ssshout (enm_active, int_layer_id, date_when_shouted, int_ssshout_id, var_whisper_to, var_ip); CREATE INDEX ordered_ssshout_big_id ON tbl_ssshout (enm_active, int_layer_id, date_when_shouted, int_ssshout_id, var_whisper_to, var_ip, int_author_id, int_whisper_to_id); ALTER TABLE tbl_ssshout DROP INDEX ordered_ssshout_full; ALTER TABLE tbl_ssshout DROP INDEX ordered_ssshout_big;";
+		
+		$sql = "CREATE INDEX ordered_ssshout_id ON tbl_ssshout (enm_active, int_layer_id, date_when_shouted, int_ssshout_id, var_whisper_to, var_ip)";
 		dbquery($sql) or die("Sorry there was an error: unable to execute query. " . dberror());
+		
+		$sql = "CREATE INDEX ordered_ssshout_big_id ON tbl_ssshout (enm_active, int_layer_id, date_when_shouted, int_ssshout_id, var_whisper_to, var_ip, int_author_id, int_whisper_to_id)";
+		dbquery($sql) or die("Sorry there was an error: unable to execute query. " . dberror());
+		
+		$sql = "ALTER TABLE tbl_ssshout DROP INDEX ordered_ssshout_full";
+		dbquery($sql) or die("Sorry there was an error: unable to execute query. " . dberror());
+		
+		$sql = "ALTER TABLE tbl_ssshout DROP INDEX ordered_ssshout_big";
+		dbquery($sql) or die("Sorry there was an error: unable to execute query. " . dberror());
+		
+		file_put_contents($maiden_check, "Indexes have been run once.");
+		
 		
 	}
 
