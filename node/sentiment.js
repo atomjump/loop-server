64688352c61ba27e3470e078fbb8eb3fd375652a
AtomJump <peter@atomjump.com>
2021-01-18 11:06:55 +1300

    fixed sentiment to new api

--- node/sentiment.js
+++ node/sentiment.js
@@ -4,16 +4,6 @@ var mysql = require('mysql');
 var os = require('os');
 var fs = require('fs');
 
-
-var config = JSON.parse(fs.readFileSync(__dirname + '/../config/config.json'));
-
-if((process.argv[2]) && (process.argv[2] == '-production')){
-  var cnf = config.production;
-} else {
-  var cnf = config.staging;
-}
- 
-
 /*
 
 crontab -e 
@@ -21,53 +11,98 @@ crontab -e
 */
 //  */1 * * * * /usr/bin/nodejs /yourserverpath/node/sentiment.js
 //  */1 * * * * /usr/bin/nodejs /yourserverpath/node/sentiment.js -production
+//  */1 * * * * /usr/bin/nodejs /yourserverpath/node/sentiment.js -production -labelregexp ^api1
 
+
+
+var config = JSON.parse(fs.readFileSync(__dirname + '/../config/config.json'));
+
+var cnf = config.staging;
+var db = cnf.db;
+var labelRegExp = null;
+if(process.argv[2]){
+  //Loop through each arg to see if (process.argv[n] == '-production')
+  for(var cnt; cnt< process.argv.length; cnt++) {
+   
+	 if(process.argv[cnt] == '-production') {
+	  	cnf = config.production;
+	 }
+	 
+	 if(process.argv[cnt] == '-labelregexp') {
+	 	if(process.argv[cnt + 1]) {
+	 		labelRegExp = process.argv[cnt + 1];
+	 	}
+	 }
+  }
+} 
+ 
+
+
+function checkDatabase(connection) { 
+	connection.query('SELECT int_ssshout_id, var_shouted FROM tbl_ssshout WHERE flt_sentiment IS NULL', function(err, rows, fields) {
+  
+  
+	  if (err) throw err;
+  
+  
+	  async.forEachLimit(rows, 5, function(row, cb) {
+  
+	  
+		  var snt = sentiment(row.var_shouted);
+		  console.log('Sentiment: ' + snt.score);
+	  
+	  
+		  connection.query('UPDATE tbl_ssshout SET flt_sentiment = ' + snt.score + ' WHERE int_ssshout_id = ' + row.int_ssshout_id, function( err) {
+			
+				if(err) {
+				   console.log('ERR:' + err);
+				   cb(err);
+				} else {
+				   cb(null);
+				}
+		  });
+	  
+	  
+	  }, function(err) {
+	 
+		 if(err) {
+			console.log('ERR:' + err);
+		 } else {
+		   console.log('Completed all sentiments!');
+		 }        
+		 //have completed all queries
+		 connection.end();
+	 
+	  });
+	});
+}
+ 
+
+
+if(labelRegExp) {
+	//Try to match the db with one of the scaleup database options
+	for(var cnt = 0; cnt<cnf.db.scaleUp.length; cnt++) {
+		if(cnf.db.scaleUp[cnt].labelRegExp == labelRegExp) {
+			//Then switch over to this scaleUp database.
+			console.log("Using scaleUp database " + labelRegExp);
+			db = cnf.db.scaleUp[cnt];
+		}
+		
+	}
+
+}
 
 var connection = mysql.createConnection({
-  host     : cnf.db.hosts[0],
-  user     : cnf.db.user,
-  password : cnf.db.pass,
-  database : cnf.db.name
+  host     : db.hosts[0],
+  user     : db.user,
+  password : db.pass,
+  database : db.name
 });
  
 connection.connect();
+checkDatabase(connection);
 
 
- 
-connection.query('SELECT int_ssshout_id, var_shouted FROM tbl_ssshout WHERE flt_sentiment IS NULL', function(err, rows, fields) {
-  
-  
-  if (err) throw err;
-  
-  
-  async.forEachLimit(rows, 5, function(row, cb) {
-  
-  	  
-      var snt = sentiment(row.var_shouted);
-      console.log('Sentiment: ' + snt.score);
-      
-      
-      connection.query('UPDATE tbl_ssshout SET flt_sentiment = ' + snt.score + ' WHERE int_ssshout_id = ' + row.int_ssshout_id, function( err) {
-            
-            if(err) {
-               console.log('ERR:' + err);
-               cb(err);
-            } else {
-               cb(null);
-            }
-      });
-      
-      
-  }, function(err) {
-     
-     if(err) {
-        console.log('ERR:' + err);
-     } else {
-       console.log('Completed all sentiments!');
-     }        
-     //have completed all queries
-     connection.end();
-     
-  });
-});
- 
+
+
+
