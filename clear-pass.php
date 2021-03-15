eb160185cafa8dd4ffc905d47216efc1f6050a32
AtomJump <peter@atomjump.com>
2021-03-15 13:14:33 +1300

    corrected location redirection on password reset

--- clear-pass.php
+++ clear-pass.php
@@ -39,7 +39,9 @@ if(isset($cnf['email']['sending']['vendor']['mailgun']['key'])) {
           $sql = "UPDATE tbl_user SET var_pass = NULL WHERE var_email = '" . $_SESSION['temp-email'] . "'";
       	  $result = dbquery($sql)  or die("Unable to execute query $sql " . dberror());
 			
-		   $target_url = add_trailing_slash(add_subdomain_to_path($cnf['serviceHome']));	        
+			
+		   $main_message = $msg['msgs'][$lang]['pass']['title'];
+		          
 		       	
            header("Location: " . $target_url);
           //Password cleared
