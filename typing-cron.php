1f84c9790d0dd761a0c13adfef45d79aa874a7ff
AtomJump <peter@atomjump.com>
2021-11-11 10:46:00 +1300

    tweak for modern mariadb,mysql databases when writing an enum

--- typing-cron.php
+++ typing-cron.php
@@ -10,7 +10,7 @@
 
 	include_once(__DIR__ . '/config/db_connect.php');		
 
-	$sql = "UPDATE tbl_ssshout SET enm_active = false where enm_active = true and enm_status = 'typing' and date_when_shouted < DATE_SUB(NOW(),INTERVAL 15 MINUTE)";
+	$sql = "UPDATE tbl_ssshout SET enm_active = 'false' where enm_active = true and enm_status = 'typing' and date_when_shouted < DATE_SUB(NOW(),INTERVAL 15 MINUTE)";
 	dbquery($sql) or die("Unable to execute query $sql " . dberror());
 	
 ?>
