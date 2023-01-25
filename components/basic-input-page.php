fa3296d41a5bda948078f8ae042e74e19b27e351
atomjump <peter@atomjump.com>
2023-01-25 14:14:37 +1300

    an input component page for clearing the password

--- components/basic-input-page.php
+++ components/basic-input-page.php
@@ -465,7 +465,7 @@
 				 				 <input type="hidden" name="devicetype" id="device-type" value="<?php echo $_REQUEST['devicetype'] ?>">
 				 				 <input type="hidden" name="date-owner-start" value="<?php echo $date_start ?>">
 				 				 <input type="hidden" id="email-modified" name="email_modified" value="false">
-				 				 <?php $sh->call_plugins_settings(null); //User added plugins here ?>								
+				 				 <?php $plugin_allowed_array = ["change_language"]; $sh->call_plugins_settings($plugin_allowed_array); //User added plugins here ?>								
 				 				
 				 				 <a id="change-lang-button"><img style="margin-top: 10px; margin-bottom: 14px;" src='front-end/img/refresh.png' width='60' height='60'></a> <img src="images/flags.png" width="48" height="14"><br/>
 								 <div class="form-group">
